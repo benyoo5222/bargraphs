9a5bcc8bd4774d194f64a6424aeca74c69b0af24
Jae Hoon Yoo <jaehoonyoo@Jaes-Mac-mini.local>
2021-05-31 22:13:06 -0400

    Showing bars using default data

--- index.js
+++ index.js
@@ -10,21 +10,49 @@ const createAPIResponses = (statusCode, message) => {
   };
 };
 
+const parseWidthOrHeightPixels = (dimensions) => {
+  return dimensions.match(/[0-9]/g).join("");
+};
+
 const checkIfBarInfoValid = (currentValue) => {
   return currentValue.value
     ? !Number.isNaN(Number(currentValue.value))
     : !Number.isNaN(Number(currentValue));
 };
 
-const createBarDivs = (barInfo, barGraphContinaerConfig) => {
+const createBarDivs = (barInfo) => {
   const fullCopyOfBarInfo = JSON.parse(JSON.stringify(barInfo.arrayOfInfo));
-  const largestNumber = fullCopyOfBarInfo.sort((a, b) => {
+  const largestDataPoint = fullCopyOfBarInfo.sort((a, b) => {
     if (barInfo.isArrayOfObjects) {
       return b.value - a.value;
     }
 
     return b - a;
   })[0];
+
+  const averageWidthOfPixelsPerGroup = Math.round(
+    parseWidthOrHeightPixels(barInfo.barGraphContinaerConfig.width) /
+      largestDataPoint
+  );
+  const averageHeightOfPixelsPerGroup = Math.round(
+    parseWidthOrHeightPixels(barInfo.barGraphContinaerConfig.height) /
+      barInfo.arrayOfInfo.length
+  );
+
+  const divBarContinerElement = document.createElement("div");
+
+  barInfo.arrayOfInfo.forEach((info) => {
+    // To Do --> Check if array of objects
+    const divElement = document.createElement("div");
+    divElement.style.backgroundColor = "black"; // Will change based on config
+    divElement.style.margin = "5px"; // Need to calculate Or be overwritten by the config
+    divElement.style.width = `${averageHeightOfPixelsPerGroup * info}px`;
+    divElement.style.height = `${averageHeightOfPixelsPerGroup}px`;
+
+    divBarContinerElement.appendChild(divElement);
+  });
+
+  return divBarContinerElement;
 };
 
 const drawBarChart = (data, options, element) => {
@@ -32,7 +60,7 @@ const drawBarChart = (data, options, element) => {
   // options represent the entire bar graph ex: title, width & height of the whole container, etc
   // element
 
-  return new Promise(resolve, (reject) => {
+  return new Promise((resolve, reject) => {
     const selectedElement = document.querySelector(element);
 
     if (selectedElement === null) {
@@ -46,19 +74,26 @@ const drawBarChart = (data, options, element) => {
       );
     }
 
-    if (!data.every(checkIfBarInfoValid)) {
+    const isArrayOfObjects = data.every(checkIfBarInfoValid);
+
+    if (!isArrayOfObjects) {
       reject(
         createAPIResponses(400, "Please make sure your bar data is valid")
       );
     }
 
-    console.log("test");
+    // To Do create Validation module for options object
 
     const barInfo = {
       barGraphContinaerConfig: {
         ...defaultOptions,
         ...options,
       },
+      arrayOfInfo: data,
+      isArrayOfObjects,
     };
+    selectedElement.appendChild(createBarDivs(barInfo));
   });
 };
+
+drawBarChart([1, 2, 3, 4, 5], {}, "#root");
