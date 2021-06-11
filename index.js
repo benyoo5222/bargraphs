fe090120bfe79504cd5ffebc1c422bae84810451
Jae Hoon Yoo <jaehoonyoo@Jaes-Mac-mini.local>
2021-06-10 21:24:45 -0400

    Vertical Bars

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const defaultOptions = {
-  width: "500px",
-  height: "500px",
-  margin: "5px",
+  graphStyle: {
+    width: "1000px",
+    height: "1000px",
+    margin: "5px",
+  },
   defaultBarColor: "black",
 };
 
@@ -50,6 +52,84 @@ const calculateNestedObjLargestValue = (arrayOfInfo) => {
   return barInfoTracker.largestNumber;
 };
 
+const createVerticalBars = (
+  graphDiv,
+  barInfo,
+  averageWidthOfPixelsPerGroup,
+  largestDataPoint
+) => {
+  const numberOfBars = barInfo.barGraphContinaerConfig.graphInfo.dataInterval
+    ? Math.floor(
+        largestDataPoint /
+          barInfo.barGraphContinaerConfig.graphInfo.dataInterval
+      )
+    : Math.floor(largestDataPoint / barInfo.arrayOfInfo.length);
+  const widthValue = barInfo.barGraphContinaerConfig.graphInfo.dataInterval
+    ? parsePixelsIntoNumber(barInfo.barGraphContinaerConfig.graphStyle.width) /
+      (largestDataPoint /
+        barInfo.barGraphContinaerConfig.graphInfo.dataInterval)
+    : parsePixelsIntoNumber(barInfo.barGraphContinaerConfig.graphStyle.width) /
+      numberOfBars;
+
+  for (let i = 0; i < numberOfBars; i++) {
+    const verticalBar = document.createElement("div");
+    verticalBar.style.boxSizing = "border-box";
+    verticalBar.style.height = "100%";
+    verticalBar.style.width = "1px";
+    verticalBar.style.borderLeft = "1px dotted black";
+    verticalBar.style.left = `${widthValue * (i + 1)}px`;
+    verticalBar.style.zIndex = "-1";
+    verticalBar.style.position = "absolute";
+
+    const valueLabel = document.createElement("div");
+    valueLabel.style.whiteSpace = "pre";
+    valueLabel.style.position = "absolute";
+    valueLabel.style.left = `${widthValue * (i + 1) - 10}px`;
+    valueLabel.style.bottom = `-20px`;
+    valueLabel.appendChild(
+      document.createTextNode(
+        barInfo.barGraphContinaerConfig.graphInfo.dataInterval
+          ? `${
+              barInfo.barGraphContinaerConfig.graphInfo.dataInterval * (i + 1)
+            } ${barInfo.barGraphContinaerConfig.graphInfo.dataIntervalUnit}`
+          : `${(largestDataPoint / numberOfBars) * (i + 1)} \n${
+              barInfo.barGraphContinaerConfig.graphInfo.dataIntervalUnit
+            }`
+      )
+    );
+
+    graphDiv.appendChild(verticalBar);
+    graphDiv.appendChild(valueLabel);
+  }
+};
+
+const createGraphContainerElement = (
+  barInfo,
+  averageWidthOfPixelsPerGroup,
+  largestDataPoint
+) => {
+  const divBarContinerElement = document.createElement("div");
+
+  divBarContinerElement.style.position = "relative";
+
+  divBarContinerElement.style.width =
+    barInfo.barGraphContinaerConfig.graphStyle.width;
+  divBarContinerElement.style.height =
+    barInfo.barGraphContinaerConfig.graphStyle.height;
+
+  divBarContinerElement.style.borderLeft = `1px solid black`; // TO DO: Change with dynamic data
+  divBarContinerElement.style.boxSizing = "border-box";
+
+  createVerticalBars(
+    divBarContinerElement,
+    barInfo,
+    averageWidthOfPixelsPerGroup,
+    largestDataPoint
+  );
+
+  return divBarContinerElement;
+};
+
 const createBarDivs = (barInfo) => {
   const fullCopyOfBarInfo = JSON.parse(JSON.stringify(barInfo.arrayOfInfo));
   const largestDataPoint = barInfo.isBarDataAnArrayOfObjects
@@ -57,29 +137,29 @@ const createBarDivs = (barInfo) => {
     : fullCopyOfBarInfo.sort((a, b) => {
         return b - a;
       })[0];
-  console.log("largest data", largestDataPoint);
 
   const averageWidthOfPixelsPerGroup = Math.round(
-    parsePixelsIntoNumber(barInfo.barGraphContinaerConfig.width) /
+    parsePixelsIntoNumber(barInfo.barGraphContinaerConfig.graphStyle.width) /
       largestDataPoint
   );
 
   const averageHeightOfPixelsPerGroup =
     Math.round(
-      parsePixelsIntoNumber(barInfo.barGraphContinaerConfig.height) /
+      parsePixelsIntoNumber(barInfo.barGraphContinaerConfig.graphStyle.height) /
         barInfo.arrayOfInfo.length
     ) -
     barInfo.arrayOfInfo.length *
-      parsePixelsIntoNumber(barInfo.barGraphContinaerConfig.margin);
+      parsePixelsIntoNumber(barInfo.barGraphContinaerConfig.graphStyle.margin);
 
-  const divBarContinerElement = document.createElement("div");
-
-  divBarContinerElement.style.width = barInfo.barGraphContinaerConfig.width;
-  divBarContinerElement.style.height = barInfo.barGraphContinaerConfig.height;
+  const divBarContinerElement = createGraphContainerElement(
+    barInfo,
+    averageWidthOfPixelsPerGroup,
+    largestDataPoint
+  );
 
   barInfo.arrayOfInfo.forEach((info, index) => {
     const divElement = document.createElement("div");
-    divElement.style.margin = barInfo.barGraphContinaerConfig.margin;
+    divElement.style.margin = `${barInfo.barGraphContinaerConfig.graphStyle.margin} 0`;
 
     if (barInfo.isBarDataAnArrayOfObjects) {
       info.arrayOfBarGroupInfo.forEach((infoObj) => {
@@ -100,14 +180,21 @@ const createBarDivs = (barInfo) => {
       return;
     }
 
-    divElement.style.backgroundColor = barInfo.barGraphContinaerConfig.barColors
-      ? barInfo.barGraphContinaerConfig.barColors[index]
-      : barInfo.barGraphContinaerConfig.defaultBarColor; // Will change based on config
+    divElement.style.backgroundColor = barInfo.barGraphContinaerConfig
+      .barViewData
+      ? barInfo.barGraphContinaerConfig.barViewData[index].barColor
+      : barInfo.barGraphContinaerConfig.defaultBarColor;
     divElement.style.width = `${
-      averageWidthOfPixelsPerGroup * info -
-      parsePixelsIntoNumber(barInfo.barGraphContinaerConfig.margin)
+      averageWidthOfPixelsPerGroup * info >
+      parsePixelsIntoNumber(barInfo.barGraphContinaerConfig.graphStyle.width)
+        ? barInfo.barGraphContinaerConfig.graphStyle.width
+        : averageWidthOfPixelsPerGroup * info
     }px`;
+
     divElement.style.height = `${averageHeightOfPixelsPerGroup}px`;
+
+    const valueText = document.createTextNode(info);
+    divElement.appendChild(valueText);
 
     divBarContinerElement.appendChild(divElement);
   });
@@ -147,10 +234,15 @@ const drawBarChart = (data, options, element) => {
       barGraphContinaerConfig: {
         ...defaultOptions,
         ...options,
+        graphStyle: {
+          ...defaultOptions.graphStyle,
+          ...options.graphStyle,
+        },
       },
       arrayOfInfo: data,
       isBarDataAnArrayOfObjects: data.every(isBarDataAnArrayOfObjects),
     };
+
     selectedElement.appendChild(createBarDivs(barInfo));
 
     resolve(createAPIResponses(200, "Sucessfully created Bar Graph"));
@@ -158,21 +250,72 @@ const drawBarChart = (data, options, element) => {
 };
 
 drawBarChart(
-  [
-    {
-      arrayOfBarGroupInfo: [
-        { barColor: "grey", value: 16 },
-        { barColor: "orange", value: 10 },
-        { barColor: "pink", value: 10 },
-      ],
+  //   [
+  //     {
+  //       arrayOfBarGroupInfo: [
+  //         { barColor: "grey", value: 16 },
+  //         { barColor: "orange", value: 10 },
+  //         { barColor: "pink", value: 10 },
+  //       ],
+  //     },
+  //     {
+  //       arrayOfBarGroupInfo: [
+  //         { barColor: "red", value: 35 },
+  //         { barColor: "blue", value: 22 },
+  //       ],
+  //     },
+  //   ],
+  [10, 20, 30],
+  {
+    barViewData: [
+      {
+        barColor: "red",
+        valueInfo: {
+          position: "top",
+          color: "white",
+          fontSize: "20px",
+        },
+        label: {
+          value: "ABC",
+          color: "blue",
+        },
+      },
+      {
+        barColor: "blue",
+        valueInfo: {
+          position: "top",
+          color: "white",
+          fontSize: "20px",
+        },
+        label: {
+          value: "ABC",
+          color: "blue",
+        },
+      },
+      {
+        barColor: "orange",
+        valueInfo: {
+          position: "top",
+          color: "white",
+          fontSize: "20px",
+        },
+        label: {
+          value: "ABC",
+          color: "blue",
+        },
+      },
+    ],
+    graphStyle: {
+      width: "900px",
+      height: "500px",
     },
-    {
-      arrayOfBarGroupInfo: [
-        { barColor: "red", value: 35 },
-        { barColor: "blue", value: 22 },
-      ],
+    graphInfo: {
+      oppositeAxisLabel: "Number of X",
+      barDirection: "Horizontal",
+      title: "Ben's Test",
+      dataInterval: "5",
+      dataIntervalUnit: "points",
     },
-  ],
-  { barColors: ["red", "pink", "blue", "orange", "black"] },
+  },
   "#root"
 );
